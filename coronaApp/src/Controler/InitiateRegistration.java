dff5fc211284376c966fa5e3c71786b8dccbaaa1
kaani65 <kaan65md@hotmail.de>
2020-07-01 21:27:18 +0200

    Newest  folder

---
+++ coronaApp/src/Controler/InitiateRegistration.java
@@ -0,0 +1,14 @@
+package Controler;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class InitiateRegistration {
+
+
+}
---
+++ coronaApp/src/View/InitiateSearch.java
@@ -0,0 +1,20 @@
+package View;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class InitiateSearch {
+
+	public void InitiateSearch(JFrame frame, JPanel panel) {
+		// TODO Auto-generated method stub
+		
+		/*button = new JButton("Login");
+		button.setBounds(10, 550, 80, 25);
+		button.addActionListener(new ActionListener(){
+
+		}*/
+	}
+}
